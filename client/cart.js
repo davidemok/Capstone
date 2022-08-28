13404c10fa9b2f274ea26584b591d48b5bee677b
David Mok <davidemok@gmail.com>
2022-08-28 12:09:13 -0500

    added total price

--- client/cart.js
+++ client/cart.js
@@ -1,13 +1,17 @@
 let btn = document.querySelector('#cart')
 const cartList = document.querySelector('#cartList')
+const price = document.querySelector('#price')
 
 const URL = 'http://localhost:4000'
 
 function getTable() {
     cartList.innerHTML = ''
+    price.innerHTML = ''
     axios.get(`${URL}/cart`)
         .then(res => {
+            let total = 0;
             res.data.forEach(elem => {
+                
                 let cartCard = `<div class="cart-card">
                     <h2>${elem.product}</h2>
                     <h3>Price: ${elem.price}</h3>
@@ -15,7 +19,12 @@ function getTable() {
                     </div>
                 `
                 cartList.innerHTML += cartCard
+                total += (elem.price * elem.quantity)
             })
+            let totalCard = `<div id="totalCard">
+            <h1>Total Price ${total}</h1>
+            </div>`
+            price.innerHTML = totalCard
         })
 }
 
